3240646bc595472513e800e2d02700bba82d4544
Aline Soares <aline.soaresss@hotmail.com>
2019-10-24 17:17:22 -0300

    up

--- src/pages/sendDocuments/sendDocuments.js
+++ src/pages/sendDocuments/sendDocuments.js
@@ -10,30 +10,35 @@ export default class sendDocument extends Component {
         super();
         this.state = {
             id: '',
-            list: [{ id: '', FieldName: '', fieldType: '', status: false, required: false, values: [], answer:[]}],
-            //file: '',
-            answer:[{ nomeCampo : '', descricaoCampo: '' }]          
+            field: [{ id: '', fieldName: '', fieldType: '', visible: false, required: false, values: [] }],
+            file: '',
+            answer: { title: '', description: '', fieldName: '' }
         }
 
         this.updateState = this.updateState.bind(this);
-        this.updateStateNomeCampoForm = this.updateStateNomeCampoForm.bind(this);
-        this.updateStateDescricaoCampoForm = this.updateStateDescricaoCampoForm.bind(this);
+        this.updateStateTitle = this.updateStateTitle.bind(this);
+        this.updateStateDescription = this.updateStateDescription.bind(this);
     }
     handleChange(selectorFiles) {
         console.log(selectorFiles)
     }
-    updateState(event) {
-        this.setState({ [event.target.name]: event.target.value })
+    updateState = (event) => {
+        this.setState({
+            answer: {
+                [event.target.name]: event.target.value
+            }
+        })
+        console.log(this.state)
     }
 
-    updateStateNomeCampoForm(event){
-        this.setState({nomeCampo: event.target.value})
+    updateStateTitle(event) {
+        this.setState({ title: event.target.value })
     }
 
-    updateStateDescricaoCampoForm(event){
-        this.setState({descricaoCampo : event.target.value})
+    updateStateDescription(event) {
+        this.setState({ description: event.target.value })
     }
-    
+
     searchFields() {
         fetch('https://5d8289a9c9e3410014070b11.mockapi.io/document', {
             headers: {
@@ -41,28 +46,32 @@ export default class sendDocument extends Component {
             }
         })
             .then(response => response.json())
-            .then(data => this.setState({ list: data }))
+            .then(data => this.setState({ field: data }))
             .catch(error => console.log(error))
     }
 
     registerDocument(event) {
         event.preventDefault();
-        fetch('https://5d8289a9c9e3410014070b11.mockapi.io/document', {
+
+
+
+        fetch('http://5d8289a9c9e3410014070b11.mockapi.io/respostaDocument', {
             method: 'POST',
             body: JSON.stringify({
-                //file: this.state.file,
-                    answer:{nomeCampo : this.state.nomeCampo,
-                    descricaoCampo : this.state.descricaoCampo
-                    }}),
+                file: this.state.file,
+                answer: this.state.answer
+            }),
             headers: {
                 'Content-Type': 'application/json'
             }
         })
-        .then(response => response)
-        .then(this.searchFields())
-        .catch(error => console.log(error))
+            .then(response => response)
+            .then(this.searchFields())
+            .catch(error => console.log(error))
     }
-    
+
+
+
     componentDidMount() {
         this.searchFields();
     }
@@ -75,26 +84,26 @@ export default class sendDocument extends Component {
                 <Menu />
                 <form onSubmit={this.registerDocument.bind(this)} className="form">
                     <div className="inputFixo">
-                    <input placeholder="Nome do Campo*" value={this.state.nomeCampo} onChange={this.updateStateNomeCampoForm} className="nomeCampo"/>
-                        <textarea placeholder="Descrição..." value={this.state.descricaoCampo} onChange={this.updateStateDescricaoCampoForm} className="descricao"/>
+                        <input placeholder="Título*" value={this.state.title} onChange={this.updateState} name="title" className="nomeCampo" />
+                        <textarea placeholder="Descrição*" value={this.state.description} onChange={this.updateState} name="description" className="descricao" />
                     </div>
-                    
+
                     {
-                        this.state.list.map(function (document) {
+                        this.state.field.map((document) => {
                             return (
                                 <div className="inputMovel">
                                     <div>
                                         <li className="lista">
-                                            <input placeholder={document.FieldName} name={document.FieldName} type={document.fieldType} className={document.fieldType} />
+                                            <input placeholder={document.fieldName} name={document.fieldName} type={document.fieldType} className="text" onChange={this.updateState} />
                                         </li>
                                     </div>
                                 </div>
                             );
                         }
-                        
+
                         )
                     }
-                    <input type="file" name="file" onChange={(e)  => this.handleChange(e.target.files)} onChange={this.updateState} />
+                    <input type="file" name="file" className="input-file" onChange={(e) => this.handleChange(e.target.files)} />
                     <div className="buttons">
                         <button className="cancel">Cancelar</button>
                         <button type="submit" className="send">Cadastrar</button>
